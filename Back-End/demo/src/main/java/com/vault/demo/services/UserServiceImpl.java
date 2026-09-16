674fca528d90fa31b98e5c94ab2e9a5c1e490066
sovereignuniversity20-art <sovereign.university20@gmail.com>
2026-09-16 09:44:43 -0500

    UserServiceImpl - checking for email and creating user completed

--- Back-End/demo/src/main/java/com/vault/demo/services/UserServiceImpl.java
+++ Back-End/demo/src/main/java/com/vault/demo/services/UserServiceImpl.java
@@ -1,4 +1,39 @@
 package com.vault.demo.services;
 
-public class UserServiceImpl {
+import com.vault.demo.dto.UserRequestDTO;
+import com.vault.demo.dto.UserResponseDTO;
+import com.vault.demo.exceptions.ItemAlreadyExistsException;
+import com.vault.demo.models.User;
+import com.vault.demo.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class UserServiceImpl  {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
+    }
+
+    public UserResponseDTO registerUser(UserRequestDTO userRequestDTO) {
+        if (userRepository.findByEmail(userRequestDTO.getEmail()).isPresent()) {
+            throw new ItemAlreadyExistsException("User already exists with this email:" + userRequestDTO.getEmail());
+        }
+            User user = modelMapper.map(userRequestDTO, User.class);
+            user.setPassword(passwordEncoder.encode(userRequestDTO.getPassword()));
+            User savedUser = userRepository.save(user);
+
+        return modelMapper.map(savedUser, UserResponseDTO.class);
+
+    }
 }
+
